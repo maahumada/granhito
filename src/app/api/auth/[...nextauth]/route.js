838e2122c95d3c502fa81cdc4db1ac49ebd331d9
MANUEL AHUMADA <maahumada@itba.edu.ar>
2024-04-06 11:24:38 -0300

    Add build dependency to package.json

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ,
+      clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
