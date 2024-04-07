34413b6b9ed86d791e51990d96ec01f38f5ac565
MANUEL AHUMADA <maahumada@itba.edu.ar>
2024-04-06 21:36:46 -0300

    Fix next config for build

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,7 @@
 const nextConfig = {
   compiler: {
     styledComponents: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'contadoresenred.com',
-        port: '',
-        pathname: '',
-      },
-    ],
-  },
+  }
 };
 
 export default nextConfig;
