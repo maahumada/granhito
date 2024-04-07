ab1e3c9e1cbae775e5966830382c3995f3de0620
MANUEL AHUMADA <maahumada@itba.edu.ar>
2024-04-06 22:26:36 -0300

    edit middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/crear", "/hito/:id*"] }   // agregar con ,
+export const config = { matcher: ["/crear"] }   // agregar con ,
